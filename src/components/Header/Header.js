97bdf28619b1cb2bbcc5e3e0c1ffb64876e113cd
jttrevis <jr.trevis@hotmail.com>
2023-04-15 18:19:17 +0100

    fontes e images

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = () => {
+	return (
+		<header>
+			<nav className="text-3xl  bg-darkGray font-aclonica">
+				<div>
+					<h1 className="text-white">
+						Greek <span className="text-green">Bite</span>
+					</h1>
+				</div>
+			</nav>
+		</header>
+	);
+};
+
+export default Header;
