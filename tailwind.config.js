82eb17827b4bdb55df1f174ed51c9bcb554dd8b0
jttrevis <jr.trevis@hotmail.com>
2023-04-18 11:57:17 +0100

    mobile menu, resposive pages home/about

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,19 @@ module.exports = {
 		'./src/app/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
+		screens: {
+			sm: '768px',
+			// => @media (min-width: 768px) { ... }
+
+			md: '1024px',
+			// => @media (min-width: 1024px) { ... }
+
+			lg: '1280px',
+			// => @media (min-width: 1280px) { ... }
+
+			xl: '1440px',
+			// => @media (min-width: 1536px) { ... }
+		},
 		extend: {
 			fontFamily: {
 				aclonica: ['var(--aclonica-font)'],
